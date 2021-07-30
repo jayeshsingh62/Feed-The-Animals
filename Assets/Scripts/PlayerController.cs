7060f8dd3e6df627ac66d14847335516add6df32
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-30 17:46:33 +0530

    Added the ability to launch projectile from the player by pressing spacebar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); // Launch the projectile from the player
         }
     }
 }
