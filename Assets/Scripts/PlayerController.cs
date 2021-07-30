2bf10b1346ec8c3bece1b62eab9ed8194853d55e
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-30 17:26:53 +0530

    Added a Gameobject variable to give the reference to food

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float speed = 10.0f;
     private float xRange = 10.0f;
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
