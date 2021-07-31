3d08f694174e919cdbd8e3c16ece0f9ab25930f4
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-31 16:34:58 +0530

    Changed the spawn interval to be a random range between 3 and 5 seconds

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,11 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(3,5));
     }
 
     // Spawn random ball at random x position at top of play area
