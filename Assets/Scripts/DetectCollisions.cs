523ea8af9659f658b8c0105d6f9ffd4edc34215b
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-02 19:17:21 +0530

    Get reference to game manager in detect collision script

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
+    public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
