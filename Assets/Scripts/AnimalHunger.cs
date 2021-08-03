ea1c386eeb4c7ab20725175f1c7d1afff5fa7b05
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-03 17:13:31 +0530

    Added the UI namespace and some variables

--- Assets/Scripts/AnimalHunger.cs
+++ Assets/Scripts/AnimalHunger.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimalHunger : MonoBehaviour
 {
+    public Slider hungerSlider;
+    public int amountToBeFed;
+
+    private int currentFedAmount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
